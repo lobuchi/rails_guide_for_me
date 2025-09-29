75e3857c28b37dec4da234ea9ad95315b8be3ec0
Lohit <lohituchil@gmail.com>
2025-09-29 21:36:05 +0100

    Initial commit of project files

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
+=begin
+  get '/products', to: 'products#index'
+  post '/products', to: 'products#create'
+  get '/producrs/:id', to: 'products#show'
+  get '/products/new', to: 'products#new'
+
+  get '/products/:id/edit' , to: 'products#edit'
+  patch '/products/:id', to: 'products#edit'
+  put '/products/:id' , to: 'products#update'
+
+  delete '/products/:id', to: 'products#destroy'
+
+  get "up" => "rails/health#show", as: :rails_health_check
+=end
+  root 'products#index'
+  resources :products
+end
